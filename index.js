a48cada4ca7d77dc1cccbe14d9d3384a7dde2cd7
vd89 <dixitvora@gmail.com>
2020-08-31 01:10:49 +0530

    Change

--- index.js
+++ index.js
@@ -16,13 +16,15 @@ app.use(Express.json());
 app.use(Express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
+app.use(Express.static('client/build'));
+
 // deploy static files
-if (process.env.NODE_ENV == 'production') {
-	app.use(Express.static('client/build'));
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
+// if (process.env.NODE_ENV == 'production') {
+// 	app.use(Express.static('client/build'));
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// 	});
+// }
 
 //Load Router
 app.use('/api/', route);
